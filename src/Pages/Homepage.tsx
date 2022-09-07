0ca7d65af4b456aa387d82cfbc362b925c288270
Michael Zabka <zabka.michael@gmail.com>
2022-09-07 17:01:38 +0000

    Add SC Catchers homepace copy

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -6,6 +6,14 @@ const Homepage: React.FC = () => {
 	return <>
 		<h1>SC Catchers</h1>
 		<img src={logo} className="App-logo" alt="logo" />
+		<blockquote className='blockquote'>
+			Vítejte na stránkách SC Catchers, týmu 8. Hanspaulské ligy.
+			V této soutěži působíme od podzimu roku 2004. Zakládajícími členy
+			byla parta kamarádu z Gymnázia Písnická. Zdaleka ne všichni v týmu
+			setrvali, ale našli se jiní a tak jsme tu stále. Největším úspěchem
+			našeho týmu bylo umístění na 5. místě. Naším hlavním cílem je postup do 7. ligy
+			a především bavit se fotbalem, nehádat se na hřišti a udělat si chuť na dobré pivo.
+		</blockquote>
 	</>;
 };
 export default Homepage;
